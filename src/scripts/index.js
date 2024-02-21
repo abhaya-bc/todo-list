17382996a7b1ca17c20374f2e4ede6fedb52a8a6
Abhaya <yeriz123@users.noreply.github.com>
2024-02-21 16:16:35 +0545

    colorPickerLinkedWithBackground

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
 const body = document.querySelector('.container>main');
 console.log(body);
+
 const picker = document.getElementById('colors');
+
+onload = () => {
+	picker.value = localStorage.getItem('color');
+	body.style['background-color'] = picker.value;
+};
+
 picker.addEventListener('change', (e) => {
+	localStorage.setItem('color', e.target.value);
 	body.style['background-color'] = e.target.value;
 });
